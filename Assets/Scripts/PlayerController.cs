60ccf12c514692ee388395cd06755b10f483a1e6
namsuhan0322 <rnatpakfl@gmail.com>
2024-09-09 22:05:31 +0900

    [Fix] 기존 Update에 있던 플레이어 움직임 로직 Move 함수에 옮김

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,11 @@ public class PlayerController : MonoBehaviour
     }
     
     void Update()
+    {
+        Move();
+    }
+
+    public void Move()
     {
         float xInput = Input.GetAxisRaw("Horizontal");
         float xSpeed = xInput * speed;
