c3d3f304b918d10f8dbb63ac9a892780fc873f24
Taehyeon88 <Taehyeon5376@naver.com>
2024-09-20 19:03:44 +0900

    [Feat]UI스크롤기능 추가 중
    LeanTween에섯추가, 버튼상호작용스크립트추가

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -26,38 +26,59 @@ public class UiManager : MonoBehaviour
     {
 
     }
-    private void ClearUI()
+    private void ClearUI()  //UI초기화
     {
         for (int i = 1; i < gameUI.Length - 1; i++)
         {
             gameUI[i].SetActive(false);
         }
     }
+    // 기본화면에서 활성화
     public void HomeUI()
     {
         ClearUI();
+        gameUI[0].SetActive(true);
         gameUI[1].SetActive(true);
     }
     public void LevelUI()
     {
         ClearUI();
+        gameUI[0].SetActive(true);
         gameUI[2].SetActive(true);
     }
     public void HalloweenUI()
     {
         ClearUI();
+        gameUI[0].SetActive(true);
         gameUI[3].SetActive(true);
     }
     public void StoreUI()
     {
         ClearUI();
+        gameUI[0].SetActive(true);
         gameUI[4].SetActive(true);
     }
     public void SettingUI()
     {
         ClearUI();
+        gameUI[0].SetActive(true);
         gameUI[5].SetActive(true);
     }
+    //게임씬 전으로 이동
+    public void ReadyUI_1()
+    {
+        ClearUI();
+        gameUI[0].SetActive(false);
+        gameUI[6].SetActive(true);
+    }
+    public void ReadyUI_2()
+    {
+        ClearUI();
+        gameUI[0].SetActive(false);
+        gameUI[7].SetActive(true);
+    }
+
+    //게임씬으로 이동
     public void PlayGame()
     {
         LoadingSceneController.LoadScene("GameScene");
