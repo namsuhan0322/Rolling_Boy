a06260f1d40d2f5039a563e4c3a92c1d43309ab7
namsuhan0322 <rnatpakfl@gmail.com>
2024-09-23 20:43:56 +0900

    [Feat] 시야 앞에있는거만 렌더링하고 플레이어 뒤는 날려버리게 코드 변경

--- Assets/Scripts/CoreSystem/LevelCreator.cs
+++ Assets/Scripts/CoreSystem/LevelCreator.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -9,16 +8,17 @@ public class LevelCreator : MonoBehaviour
     [SerializeField] private GameObject[] tiles;
     [SerializeField] private GameObject destinationObject;
     [SerializeField] private GameObject mainCamera;
+    public float viewDistance = 50f; // 플레이어의 시야 거리
 
-    public int levelLength { get; private set; } = 0;   
-    
+    private List<GameObject> createdTiles = new List<GameObject>();
+
     void Start()
     {
         if (GameManager.instance != null)
         {
             sourceFile = new FileInfo("Assets/Levels/Level" + GameManager.instance.currentLevel + ".txt");
         }
-        
+
         StreamReader reader = sourceFile.OpenText();
         string text = reader.ReadLine();
 
@@ -26,34 +26,43 @@ public class LevelCreator : MonoBehaviour
         {
             createRow(i, text);
             text = reader.ReadLine();
-            levelLength = i;
+        }
+
+        UpdateTileVisibility(); // 시작 시 타일 가시성 업데이트
+    }
+
+    private void Update()
+    {
+        UpdateTileVisibility(); // 매 프레임 타일 가시성 업데이트
+    }
+
+    private void createRow(int zPos, string rowInfo) 
+    {
+        for (int i = 0; i < 5; i++) 
+        {
+            int tileType = rowInfo[i] - 'a';
+            if (tileType >= 0 && tileType < tiles.Length)
+            {
+                GameObject tile = Instantiate(tiles[tileType], new Vector3(i, 0, zPos), Quaternion.identity, destinationObject.transform);
+                tile.SetActive(false); // 초기 생성 시 비활성화
+                createdTiles.Add(tile); // 생성한 타일을 리스트에 추가
+            }
         }
     }
 
-    private void createRow (int zPos, string rowInfo) 
+    private void UpdateTileVisibility()
     {
-        switch (rowInfo[0]) 
-        {
-            case 'c':
-                createTile(zPos, 0, rowInfo[0] - 'a');
-                break;
-            case 'd':
-                createTile(zPos, 0, rowInfo[0] - 'a');
-                break;
-            default:
-                for (int i = 0; i < 5; i++) 
-                {
-                    createTile(zPos, i, rowInfo[i] - 'a');
-                }
-                break;
-        }
-    }
+        Vector3 playerPosition = mainCamera.transform.position;
 
-    private void createTile (int zPos, int xPos, int tileType) 
-    {
-        if (tileType >= 0 && tileType < tiles.Length)
+        foreach (GameObject tile in createdTiles)
         {
-            Instantiate(tiles[tileType], new Vector3(xPos, 0, zPos), Quaternion.identity, destinationObject.transform);
+            float distance = Vector3.Distance(playerPosition, tile.transform.position);
+
+            // 카메라의 z축을 기준으로 타일이 뒤에 있는지 확인
+            bool isBehindPlayer = tile.transform.position.z < playerPosition.z;
+
+            // 타일의 가시성 결정: 시야 거리 이내이면서 플레이어 뒤가 아니어야 활성화
+            tile.SetActive(distance <= viewDistance && !isBehindPlayer);
         }
     }
 }
